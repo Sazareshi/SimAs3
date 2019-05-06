31ba1deb6d0546169d0f482e8051c82750ddf685
Kobayashi <masato.kobayashi@shi-g.com>
2019-05-06 21:47:49 +0900

    update 20190506

--- MultiTask/CMain.cpp
+++ MultiTask/CMain.cpp
@@ -10,6 +10,8 @@ CMain::CMain(){
 	dummy_argc = 1;
 	argv = WindowTitle;
 	dummy_argv = &argv;
+
+
 }
 CMain::~CMain(){
 }
@@ -22,6 +24,52 @@ int CMain::WindowHeight;						//生成するウィンドウの高さ
 int CMain::dummy_argc;
 char* CMain::argv;
 char** CMain::dummy_argv;
+bool CMain::_Bitmap = false;
+
+double CMain::PI = acos(-1.0);
+int CMain::tn = 0;
+double CMain::t = 0;
+double CMain::dt = 0.005;
+double CMain::omega = 2.0 * PI / 10.0;
+gl_screenshot CMain::gs; //bmpファイルの出力
+st_p CMain::p[100];
+int CMain::pn = 0;
+double CMain::ax = 0.0, CMain::ay = 0.0, CMain::az = -2.0;
+double CMain::vx = 5.0, CMain::vy = 10.0, CMain::vz = 20.0;
+double CMain::hanpatu = 0.9;
+GLfloat CMain::floor_planar[4];
+GLfloat CMain::floor_s = 50.0f;
+GLfloat CMain::pM[16];
+GLfloat CMain::lightpos[4] = { -30, -100, 50, 1 };
+QUADS_VERTEX CMain::floor_v = {{ floor_s,  floor_s, -1.0f },{ -floor_s,  floor_s, -1.0f },{ -floor_s, -floor_s, -1.0f },{ floor_s, -floor_s, -1.0f },};
+
+//直方体の定義
+GLdouble CMain::vertex[][3] = {{ 0.0, 0.0, 0.0 },{ 2.0, 0.0, 0.0 },{ 2.0, 2.0, 0.0 },{ 0.0, 2.0, 0.0 },{ 0.0, 0.0, 30.0 },{ 2.0, 0.0, 30.0 },{ 2.0, 2.0, 30.0 },{ 0.0, 2.0, 30.0 }};
+//面の定義
+int CMain::face[][4] = {{ 0, 1, 2, 3 },{ 1, 5, 6, 2 },{ 5, 4, 7, 6 },{ 4, 0, 3, 7 },{ 4, 5, 1, 0 },{ 3, 2, 6, 7 }};
+//面の法線ベクトル
+GLdouble CMain::normal[][3] = {{ 0.0, 0.0,-1.0 },{ 1.0, 0.0, 0.0 },{ 0.0, 0.0, 1.0 },{ -1.0, 0.0, 0.0 },{ 0.0,-1.0, 0.0 },{ 0.0, 1.0, 0.0 }};
+
+//----------------------------------------------------
+// 色の定義
+//----------------------------------------------------
+//jade(翡翠)
+MaterialStruct CMain::ms_jade = {{ 0.135,     0.2225,   0.1575,   1.0 },{ 0.54,      0.89,     0.63,     1.0 },{ 0.316228,  0.316228, 0.316228, 1.0 },12.8 };
+//ruby(ルビー)
+MaterialStruct CMain::ms_ruby = {{ 0.1745,   0.01175,  0.01175,   1.0 },{ 0.61424,  0.04136,  0.04136,   1.0 },{ 0.727811, 0.626959, 0.626959,  1.0 },76.8 };
+
+GLfloat CMain::red[] = { 0.8, 0.2, 0.2, 1.0 }; //赤色
+GLfloat CMain::green[] = { 0.2, 0.8, 0.2, 1.0 };//緑色
+GLfloat CMain::blue[] = { 0.2, 0.2, 0.8, 1.0 };//青色
+GLfloat CMain::yellow[] = { 0.8, 0.8, 0.2, 1.0 };//黄色
+GLfloat CMain::white[] = { 1.0, 1.0, 1.0, 1.0 };//白色
+GLfloat CMain::shininess = 30.0;//光沢の強さ
+//-----------------------------------------
+
+double CMain::ViewPointX = 0.0;
+double CMain::ViewPointY = -50.0;
+double CMain::ViewPointZ = 20.0;
+
 
 LRESULT CALLBACK  CMain::PanelProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM lp) {
 	int n;
@@ -110,10 +158,10 @@ void CMain::set_panel_tip_txt()
 	wstring wstr_type; wstring wstr;
 	switch (inf.panel_func_id) {
 	case IDC_TASK_FUNC_RADIO1: {
-		wstr = L"Type for Func1 \n\r 1:Change CUL Status 2:?? 3:?? \n\r 4:?? 5:?? 6:??";
+		wstr = L"Type for Func1 \n\r 1:?? 2:?? 3:?? \n\r 4:?? 5:?? 6:??";
 		switch (inf.panel_type_id) {
 		case IDC_TASK_ITEM_RADIO1:
-			wstr_type += L"Param of type1 \n\r 1:CUL No. 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			wstr_type += L"Param of type1 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
 			break;
 		case IDC_TASK_ITEM_RADIO2:
 			wstr_type += L"Param of type2 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
@@ -266,14 +314,23 @@ void CMain::set_panel_tip_txt()
 
 //#################### OPEN GL ################################################
 void CMain::ActOpenGL(void) {
+
+	srand((unsigned)time(NULL));
+
+	if (_Bitmap) _mkdir("bitmap"); //bmpファイル保存用のフォルダの作成
+
 	glutInit(&dummy_argc, dummy_argv);//環境の初期化
 	glutInitWindowPosition(WindowPositionX, WindowPositionY);//ウィンドウの位置の指定
 	glutInitWindowSize(WindowWidth, WindowHeight); //ウィンドウサイズの指定
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);//ディスプレイモードの指定
 	glutCreateWindow(WindowTitle);  //ウィンドウの作成
 	glutDisplayFunc(Display); //描画時に呼び出される関数を指定する（関数名：Display）
+	glutKeyboardFunc(Keyboard);//キーボード入力時に呼び出される関数を指定する（関数名：Keyboard）
+
+	glutIdleFunc(Idle);       //プログラムアイドル状態時に呼び出される関数
 	Initialize(); //初期設定の関数を呼び出す
 	glutMainLoop();
+
 	return;
 }
 
@@ -284,11 +341,19 @@ void CMain::Initialize(void) {
 	glClearColor(1.0, 1.0, 1.0, 1.0); //背景色
 	glEnable(GL_DEPTH_TEST);//デプスバッファを使用：glutInitDisplayMode() で GLUT_DEPTH を指定する
 
+	glDepthFunc(GL_LEQUAL);
+	glClearDepth(1.0);
+
+	findPlane(floor_planar,	floor_v.v0,	floor_v.v1,	floor_v.v2);
+
+	//透視変換行列の設定------------------------------
+	glMatrixMode(GL_PROJECTION);//行列モードの設定（GL_PROJECTION : 透視変換行列の設定、GL_MODELVIEW：モデルビュー変換行列）
+	glLoadIdentity();//行列の初期化
 	gluPerspective(30.0, (double)WindowWidth / (double)WindowHeight, 0.1, 1000.0); //透視投影法の視体積gluPerspactive(th, w/h, near, far);
-	gluLookAt(
-		0.0, -200.0, 50.0, // 視点の位置x,y,z;
-		0.0, 0.0, 20.0,   // 視界の中心位置の参照点座標x,y,z
-		0.0, 0.0, 1.0);  //視界の上方向のベクトルx,y,z
+
+	//光源の設定--------------------------------------
+	GLfloat light_position0[] = { -50.0, -50.0, 20.0, 1.0 }; //光源0の座標
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position0); //光源0を設定
 
 }
 //----------------------------------------------------
@@ -297,10 +362,167 @@ void CMain::Initialize(void) {
 void CMain::Display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //バッファの消去
 
+
+	t = dt * tn;
+
+	//ViewPointX = -100.0 * cos(omega * t);
+	//ViewPointY = -100.0 * sin(omega * t);
+	//ViewPointY += (-50.0 - ViewPointY) * 0.001;
+
+
+	//透視変換行列の設定------------------------------
+	glMatrixMode(GL_PROJECTION);//行列モードの設定（GL_PROJECTION : 透視変換行列の設定、GL_MODELVIEW：モデルビュー変換行列）
+	glLoadIdentity();//行列の初期化
+	gluPerspective(30.0, (double)WindowWidth / (double)WindowHeight, 0.1, 1000.0); //透視投影法の視体積gluPerspactive(th, w/h, near, far);
+
+	//視点の設定------------------------------
+	gluLookAt(
+/*
+		ViewPointX, ViewPointY, ViewPointZ, // 視点の位置x,y,z;
+		0.0, 0.0, ViewPointZ,   // 視界の中心位置の参照点座標x,y,z
+*/
+		0.0, -160.0, 40, // 視点の位置x,y,z;
+		0.0, 0.0, 0.0,   // 視界の中心位置の参照点座標x,y,z
+		0.0, 0.0, 1.0);  //視界の上方向のベクトルx,y,z
+
+	//モデルビュー変換行列の設定--------------------------
+	glMatrixMode(GL_MODELVIEW);//行列モードの設定（GL_PROJECTION : 透視変換行列の設定、GL_MODELVIEW：モデルビュー変換行列）
+	glLoadIdentity();//行列の初期化
+	glViewport(0, 0, WindowWidth, WindowHeight);
+	//----------------------------------------------
+
+	//ステンシルバッファクリア値の設定--------------------------
+	glClearStencil(0);
+	glCullFace(GL_BACK);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_AUTO_NORMAL);
+	glEnable(GL_NORMALIZE);
+	//----------------------------------------
+
+	// 平面射影行列の算出--------------------------
+	shadowMatrix(pM, floor_planar, lightpos);
+	//--------------------------
+
+	//陰影ON-----------------------------
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);//光源0を利用
+	glLightfv(GL_LIGHT0, GL_POSITION, lightpos);
+
+	//-----------------------------------
+
+#if 0
+	//球
+	glPushMatrix();
+    //glColor3d(1.0, 0.0, 0.0); //色の設定
+	glMaterialfv(GL_FRONT, GL_AMBIENT, ms_ruby.ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, ms_ruby.diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, ms_ruby.specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, &ms_ruby.shininess);
+	glTranslated(0.0, 10.0, 20.0);//平行移動値の設定
+	glutSolidSphere(4.0, 20, 20);//引数：(半径, Z軸まわりの分割数, Z軸に沿った分割数)
+	glPopMatrix();
+
+	//立方体
+	glPushMatrix();
+	//glColor3d(0.0, 1.0, 0.0);//色の設定
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, green);
+	glTranslated(-20.0, 0.0, 20.0);//平行移動値の設定
+	glutSolidCube(10.0);//引数：(一辺の長さ)
+	glPopMatrix();
+
+	//円錐
+	glPushMatrix();
+	//glColor3d(0.0, 0.0, 1.0);//色の設定
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, blue);
+	glTranslated(20.0, 100.0, 0.0);//平行移動値の設定
+	glutSolidCone(5.0, 10.0, 20, 20);//引数：(半径, 高さ, Z軸まわりの分割数, Z軸に沿った分割数)
+	glPopMatrix();
+
+	//直方体
+	glPushMatrix();
+	//glColor3d(0.0, 1.0, 1.0);//色の設定
+	glMaterialfv(GL_FRONT, GL_AMBIENT, ms_jade.ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, ms_jade.diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, ms_jade.specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, &ms_jade.shininess);
+	glTranslated(30.0, 50.0, 0.0);//平行移動値の設定
+	glBegin(GL_QUADS);
+	for (int j = 0; j < 6; ++j) {
+		glNormal3dv(normal[j]); //法線ベクトルの指定
+
+		for (int i = 0; i < 4; ++i) {
+			glVertex3dv(vertex[face[j][i]]);
+		}
+	}
+
+	glEnd();
+	glPopMatrix();
+#endif
+#if 0
+	for (int i = 1; i <= pn; i++) {
+		p[i].vx += ax * dt;
+		p[i].vy += ay * dt;
+		p[i].vz += az * dt;
+		p[i].x += p[i].vx * dt;
+		p[i].y += p[i].vy * dt;
+		p[i].z += p[i].vz * dt;
+		if (p[i].z < 4) {
+			p[i].z = 4.0;
+			p[i].vz = -hanpatu * p[i].vz;
+		}
+		glPushMatrix();
+		glMaterialfv(GL_FRONT, GL_AMBIENT, ms_ruby.ambient);
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, ms_ruby.diffuse);
+		glMaterialfv(GL_FRONT, GL_SPECULAR, ms_ruby.specular);
+		glMaterialfv(GL_FRONT, GL_SHININESS, &ms_ruby.shininess);
+		glTranslated(p[i].x, p[i].y, p[i].z);//平行移動値の設定
+		glutSolidSphere(4.0, 20, 20);//引数：(半径, Z軸まわりの分割数, Z軸に沿った分割数)
+		glPopMatrix();
+	}
+#endif
+	glPushMatrix();
+		DrawStructure(false);
+		DrawShadow();
+	glPopMatrix();
+
+	glDisable(GL_AUTO_NORMAL);
+	glDisable(GL_NORMALIZE);
+
+	//陰影OFF-----------------------------
+	glDisable(GL_LIGHTING);
+	//-----------------------------------
+
 	Ground();
 
+#if _BITMAP
+	ostringstream fname;
+	int tt = tn + 10000;
+	fname << "bitmap/" << tt << ".bmp";//出力ファイル名
+	string name = fname.str();
+	gs.screenshot(name.c_str(), 24);
+	tn++;
+
+#else
+	
+	if (_Bitmap) {
+		ostringstream fname;
+		int tt = tn + 10000;
+		fname << "bitmap/" << tt << ".bmp";//出力ファイル名
+		string name = fname.str();
+		gs.screenshot(name.c_str(), 24);
+	}
+	tn++;
+#endif
 	glutSwapBuffers(); //glutInitDisplayMode(GLUT_DOUBLE)でダブルバッファリングを利用可
 }
+
+//----------------------------------------------------
+// アイドル時に呼び出される関数
+//----------------------------------------------------
+void CMain::Idle() {
+	glutPostRedisplay(); //glutDisplayFunc()を１回実行する
+}
+
 
 //----------------------------------------------------
 // 大地の描画
@@ -311,12 +533,206 @@ void CMain::Ground(void) {
 	glColor3d(0.8, 0.8, 0.8);  // 大地の色
 	glBegin(GL_LINES);
 	for (double ly = -ground_max_y; ly <= ground_max_y; ly += 10.0) {
-		glVertex3d(-ground_max_x, ly, 0);
-		glVertex3d(ground_max_x, ly, 0);
+		glVertex3d(-ground_max_x, ly, -1.1);
+		glVertex3d(ground_max_x, ly, -1.1);
 	}
 	for (double lx = -ground_max_x; lx <= ground_max_x; lx += 10.0) {
-		glVertex3d(lx, ground_max_y, 0);
-		glVertex3d(lx, -ground_max_y, 0);
+		glVertex3d(lx, ground_max_y, -1.1);
+		glVertex3d(lx, -ground_max_y, -1.1);
 	}
 	glEnd();
 }
+
+//----------------------------------------------------
+// キーボード入力時に呼び出される関数
+//----------------------------------------------------
+void CMain::Keyboard(unsigned char key, int x, int y) {
+	switch (key)
+	{
+	case 'a':
+		pn++;
+		p[pn].x = -0.0;
+		p[pn].y = -10.0;
+		p[pn].z = 5.0;
+		p[pn].vx = vx * ((double)rand() / (double)RAND_MAX - (double)rand() / (double)RAND_MAX);
+		p[pn].vy = vy * ((double)rand() / (double)RAND_MAX);
+		p[pn].vz = vz * ((double)rand() / (double)RAND_MAX);
+
+		break;
+	case 'q':
+		exit(0);
+		break;
+
+	default:
+		break;
+	}
+}
+
+//----------------------------------------------------
+// 物体の描画
+//----------------------------------------------------
+void CMain::DrawStructure(bool flag) {
+	for (int i = 1; i <= pn; i++) {
+		p[i].vx += ax * dt;
+		p[i].vy += ay * dt;
+		p[i].vz += az * dt;
+		p[i].x += p[i].vx * dt;
+		p[i].y += p[i].vy * dt;
+		p[i].z += p[i].vz * dt;
+		if (p[i].z < 4 && abs(p[i].x) < floor_s  && abs(p[i].y) < floor_s) {
+			p[i].z = 4.0;
+			p[i].vz = -hanpatu * p[i].vz;
+		}
+		if (!flag || p[i].z >0) {
+			glPushMatrix();
+			glMaterialfv(GL_FRONT, GL_AMBIENT, ms_ruby.ambient);
+			glMaterialfv(GL_FRONT, GL_DIFFUSE, ms_ruby.diffuse);
+			glMaterialfv(GL_FRONT, GL_SPECULAR, ms_ruby.specular);
+			glMaterialfv(GL_FRONT, GL_SHININESS, &ms_ruby.shininess);
+			glTranslated(p[i].x, p[i].y, p[i].z);//平行移動値の設定
+			glutSolidSphere(4.0, 20, 20);//引数：(半径, Z軸まわりの分割数, Z軸に沿った分割数)
+			glPopMatrix();
+		}
+	}
+}
+
+
+//----------------------------------------------------
+// 床平面の方程式と行列の計算
+//----------------------------------------------------
+void CMain::findPlane(
+	GLfloat plane[4],  // 作成する平面方程式の係数
+	GLfloat v0[3],    // 頂点１
+	GLfloat v1[3],    // 頂点２
+	GLfloat v2[3])    // 頂点３
+{
+	GLfloat vec0[3], vec1[3];
+
+	// Need 2 vectors to find cross product.
+	vec0[0] = v1[0] - v0[0];
+	vec0[1] = v1[1] - v0[1];
+	vec0[2] = v1[2] - v0[2];
+
+	vec1[0] = v2[0] - v0[0];
+	vec1[1] = v2[1] - v0[1];
+	vec1[2] = v2[2] - v0[2];
+
+	// find cross product to get A, B, and C of plane equation
+	plane[0] = vec0[1] * vec1[2] - vec0[2] * vec1[1];
+	plane[1] = -(vec0[0] * vec1[2] - vec0[2] * vec1[0]);
+	plane[2] = vec0[0] * vec1[1] - vec0[1] * vec1[0];
+
+	plane[3] = -(plane[0] * v0[0] + plane[1] * v0[1] + plane[2] * v0[2]);
+}
+void CMain::shadowMatrix(
+	GLfloat *m,      // 作成する行列のポインタ
+	GLfloat plane[4],  // 射影する表面の平面方程式の係数
+	GLfloat light[4])  // 光源の同時座標値
+{
+	GLfloat dot;
+
+	// Find dot product between light position vector and ground plane normal.
+	dot = plane[0] * light[0] +
+		plane[1] * light[1] +
+		plane[2] * light[2] +
+		plane[3] * light[3];
+
+	m[0] = dot - light[0] * plane[0];
+	m[4] = 0.f - light[0] * plane[1];
+	m[8] = 0.f - light[0] * plane[2];
+	m[12] = 0.f - light[0] * plane[3];
+
+	m[1] = 0.f - light[1] * plane[0];
+	m[5] = dot - light[1] * plane[1];
+	m[9] = 0.f - light[1] * plane[2];
+	m[13] = 0.f - light[1] * plane[3];
+
+	m[2] = 0.f - light[2] * plane[0];
+	m[6] = 0.f - light[2] * plane[1];
+	m[10] = dot - light[2] * plane[2];
+	m[14] = 0.f - light[2] * plane[3];
+
+	m[3] = 0.f - light[3] * plane[0];
+	m[7] = 0.f - light[3] * plane[1];
+	m[11] = 0.f - light[3] * plane[2];
+	m[15] = dot - light[3] * plane[3];
+}
+
+//----------------------------------------------------
+// 床の描画と影の描画
+//----------------------------------------------------
+void CMain::DrawFloor(bool bTexture) {
+	if (bTexture) {
+		// 床にテクスチャを使う時はココで設定する
+		//  glBindTexture( GL_TEXTURE_2D, );
+
+		glDisable(GL_LIGHTING);
+		glBegin(GL_QUADS);
+		//    glTexCoord2f( , );
+		glVertex3fv(floor_v.v0);
+		//    glTexCoord2f( , );
+		glVertex3fv(floor_v.v1);
+		//    glTexCoord2f( , );
+		glVertex3fv(floor_v.v2);
+		//    glTexCoord2f( , );
+		glVertex3fv(floor_v.v3);
+		glEnd();
+		glEnable(GL_LIGHTING);
+	}
+	else {
+		glDisable(GL_LIGHTING);
+		glBegin(GL_QUADS);
+		glVertex3fv(floor_v.v0);
+		glVertex3fv(floor_v.v1);
+		glVertex3fv(floor_v.v2);
+		glVertex3fv(floor_v.v3);
+		glEnd();
+		glEnable(GL_LIGHTING);
+	}
+}
+void CMain::DrawShadow(void) {
+	/////////////////////////////////////////////
+	//床のステンシルを付ける
+	glEnable(GL_STENCIL_TEST);
+	glStencilFunc(GL_ALWAYS, 1, ~0);
+	//これから描画するもののステンシル値にすべて１タグをつける
+	glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+	glColor4f(0.7f, 0.4f, 0.0f, 1.0f);
+	DrawFloor(true);//床の描画
+
+					/////////////////////////////////////////////
+					//カラー・デプスバッファマスクをセットする
+					//これで以下の内容のピクセルの色の値は、書き込まれない。
+	glColorMask(0, 0, 0, 0);
+	glDepthMask(0);
+	/////////////////////////////////////////////
+	//床にオブジェクトの影のステンシルを付ける
+	glEnable(GL_STENCIL_TEST);
+	glStencilFunc(GL_EQUAL, 1, ~0);
+	//これから描画するもののステンシル値にすべて１タグをつける
+	glStencilOp(GL_KEEP, GL_KEEP, GL_INCR);
+	glDisable(GL_DEPTH_TEST);
+	glPushMatrix();
+	glMultMatrixf(pM);
+	DrawStructure(true);
+	glPopMatrix();
+	glEnable(GL_DEPTH_TEST);
+
+	/////////////////////////////////////////////
+	//ビットマスクを解除
+	glColorMask(1, 1, 1, 1);
+	glDepthMask(1);
+
+	/////////////////////////////////////////////
+	//影をつける
+	glStencilFunc(GL_EQUAL, 2, ~0);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glColor4f(0.1f, 0.1f, 0.1f, 0.5f);
+	glDisable(GL_DEPTH_TEST);
+	DrawFloor(false);//床の描画
+	glEnable(GL_DEPTH_TEST);
+	glDisable(GL_BLEND);
+	glDisable(GL_STENCIL_TEST);
+}
+
--- MultiTask/CMain.h
+++ MultiTask/CMain.h
@@ -1,5 +1,31 @@
 #pragma once
 #include "ThreadObj.h"
+
+#define _BITMAP 0
+
+//----------------------------------------------------
+// 物質質感の定義
+//----------------------------------------------------
+struct MaterialStruct {
+	GLfloat ambient[4];
+	GLfloat diffuse[4];
+	GLfloat specular[4];
+	GLfloat shininess;
+};
+
+struct st_p {
+	double x,y,z;
+	double vx, vy, vz;
+};
+
+typedef struct _QUADS_VERTEX {
+	GLfloat v0[3];
+	GLfloat v1[3];
+	GLfloat v2[3];
+	GLfloat v3[3];
+}QUADS_VERTEX;
+
+
 class CMain :	public CThreadObj
 {
 public:
@@ -9,12 +35,61 @@ public:
 	static int WindowPositionX;					//生成するウィンドウ位置のX座標
 	static int WindowPositionY;					//生成するウィンドウ位置のY座標
 	static int WindowWidth;						//生成するウィンドウの幅
-	static int WindowHeight;						//生成するウィンドウの高さ
-	static char WindowTitle[128];// = "世界の始まり"; //ウィンドウのタイト
+	static int WindowHeight;					//生成するウィンドウの高さ
+	static char WindowTitle[128];// = "世界の始まり"; //ウィンドウのタイトル
 	 // Window Appliにするためのダミー引数
 	static int dummy_argc;
 	static char* argv;
 	static char** dummy_argv;
+
+	static bool _Bitmap;
+	static int tn;
+	static double t;
+	static double dt;
+	static double omega;
+	static gl_screenshot gs; //bmpファイルの出力
+	static double PI;
+
+	//----------------------------------------------------
+	// 視点の定義
+	//----------------------------------------------------
+	static double ViewPointX;
+	static double ViewPointY;
+	static double ViewPointZ;
+	//----------------------------------------------------
+	// 直方体の定義
+	//----------------------------------------------------
+	static GLdouble vertex[][3];//頂点座標
+	static int face[][4];//面定義
+	static GLdouble normal[][3];
+
+	//----------------------------------------------------
+	// 色の定義
+	//----------------------------------------------------
+	static MaterialStruct ms_jade;//jade(翡翠)
+	static MaterialStruct ms_ruby;//ruby(ルビー)
+	static GLfloat red[]; //赤色
+	static GLfloat green[];//緑色
+	static GLfloat blue[];//青色
+	static GLfloat yellow[];//黄色
+	static GLfloat white[];//白色
+	static GLfloat shininess;//光沢の強さ
+
+	static st_p p[100];
+	static int pn;
+	static double ax, ay, az;
+	static double vx, vy, vz;
+	static double hanpatu;
+
+	#include <time.h>
+
+	static GLfloat floor_planar[4];
+	static GLfloat floor_s;
+	static GLfloat pM[16];
+	static GLfloat lightpos[4];
+	static QUADS_VERTEX floor_v;
+	
+	//-----------------------------------------
 
 	LRESULT CALLBACK PanelProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM lp);
 	void set_panel_tip_txt();//タブパネルのStaticテキストを設定
@@ -22,9 +97,20 @@ public:
 //OPEN GL---------------------------------------------
 	static void Initialize(void);
 	static void Display(void);
-	static void Ground(void);
+	static void Ground(void); //大地の描画Keyboard
+	static void Idle();
+	static void Keyboard(unsigned char key, int x, int y);
+
+	static void findPlane(GLfloat plane[4], GLfloat v0[3], GLfloat v1[3], GLfloat v2[3]);
+	static void shadowMatrix(GLfloat *m, GLfloat plane[4], GLfloat light[4]);
+	static void DrawFloor(bool bTexture);
+	static void DrawShadow(void);
+	static void DrawStructure(bool);
 
 	static void ActOpenGL(void);
+
+
+
 //OPEN GL---------------------------------------------
 
 };
